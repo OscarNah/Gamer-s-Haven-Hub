15ba08359b29c5d4a3047fd7ae91607ac615e43b
OscarNah <1im4montesdeocaoscar@gmail.com>
2024-01-15 19:06:07 -0600

    Se arreglo un error en ItemDetail

--- Gamer's Haven Hub/src/App.jsx
+++ Gamer's Haven Hub/src/App.jsx
@@ -1,22 +1,25 @@
-import './App.css'
+import "./App.css";
 
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/navBar";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar/>
-        <div className='cuerpo'>
-        <Routes>
-          <Route path='/' element={<ItemListContainer greetings="Todos los productos" />}/>
-          <Route path='/category/:categoryId' element={<ItemListContainer greetings="Productos filtrados por categoria"/>}/>
-          <Route path='/detail/:productId' element={<ItemDetailContainer/>}/>
-        </Routes>
-        </div>
+        <CartProvider>
+          <NavBar />
+          
+            <Routes>
+              <Route path="/" element={<ItemListContainer greetings="Todos los productos" />} />
+              <Route path="/category/:categoryId" element={<ItemListContainer greetings="Productos filtrados por categoria" />} />
+              <Route path="/detail/:productId" element={<ItemDetailContainer />} />
+            </Routes>
+          
+        </CartProvider>
       </BrowserRouter>
     </>
   );
--- Gamer's Haven Hub/src/components/ItemDetail/ItemDetail.jsx
+++ Gamer's Haven Hub/src/components/ItemDetail/ItemDetail.jsx
@@ -4,15 +4,15 @@ import ItemCount from "../ItemCount/ItemCount";
 const ItemDetail = ({ name, category, img, stock, price, description }) => {
   return (
     <div className="productoDetalle">
-      <div class="product-card">
+      <div className="product-card">
         <img src={img} />
-        <div class="product-details">
+        <div className="product-details">
           <h1>{name}</h1>
           <ul>
             <li>Categoria: {category}</li>
             <li>Precio: ${price}</li>
             <li>Descripcion: {description}</li>
-            <li><ItemCount stock={stock}/></li>
+            <li><ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log("cantidad agregada", quantity)} /></li>
           </ul>
         </div>
       </div>
