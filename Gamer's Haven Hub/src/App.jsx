dc2c165824b92adadade907f0b7bca96640078de
OscarNah <1im4montesdeocaoscar@gmail.com>
2024-01-18 13:54:48 -0600

    Se sube este commit para mandar a vercel

--- Gamer's Haven Hub/src/App.jsx
+++ Gamer's Haven Hub/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import NavBar from "./components/NavBar/navBar";
+import NavBar from "./components/NavBar/NavBar";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import CartView from "./components/CartView/CartView";
 import Checkout from "./components/Checkout/Checkout";
--- Gamer's Haven Hub/src/services/firebase/firebaseConfig.js
+++ Gamer's Haven Hub/src/services/firebase/firebaseConfig.js
@@ -13,3 +13,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
+
+// apiKey: import.meta.env.VITE_apikey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITEP_messagingSenderId,
+//   appId: import.meta.env.VITE_appId
